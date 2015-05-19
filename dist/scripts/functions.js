09b8f9e30a923b18a430827a164c7879d4113126
Sydney Lindsay <sydneyjlindsay@gmail.com>
2015-05-19 12:23:29 -0500

    initial commit

---
+++ dist/scripts/functions.js
@@ -0,0 +1,12 @@
+// 5 7 => 12
+// -3 0 => -3 
+// -7 12 => 5
+//all input in functions comes in as a string
+
+function add(a, b) {
+	//convert a and b to numbers 
+	// add a and b 
+	a = parseFloat(a); 
+	b = parseFloat(b);
+	return a + b; 
+}
