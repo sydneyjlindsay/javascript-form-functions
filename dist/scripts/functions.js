111c8a6068651f5427ddce1407bd844d2944f5c9
Sydney Lindsay <sydneyjlindsay@gmail.com>
2015-05-19 18:43:40 -0500

    second commit

--- dist/scripts/functions.js
+++ dist/scripts/functions.js
@@ -10,3 +10,82 @@ function add(a, b) {
 	b = parseFloat(b);
 	return a + b; 
 }
+
+function subtract(a, b) {
+	a = parseFloat(a);
+	b = parseFloat(b);
+	return a - b;
+}
+
+
+function tax(subtotal, rate) {
+	subtotal = parseFloat(subtotal);
+	rate = parseFloat(rate);
+	var tax = (subtotal*rate)/100; 
+	var total = Math.round((subtotal+tax)*100)/100; 
+	return total;
+}
+
+
+function stringLength(string) {
+	var lengthType;
+	if (string.length<5) {
+		lengthType = "short string";
+	}
+	else if(string.length>5 && string.length<=10) {
+		lengthType = "medium string";
+	}
+	else if(string.length>10) {
+		lengthType = "long string";
+	}
+	return lengthType;
+}
+
+
+function startsWith(string) {
+	var result;
+	var letter = string.charAt(0).toUpperCase();
+	if (letter === 'A') {
+		result = 'starts with A';
+	}
+	else if (letter === 'B') {
+		result = 'starts with B';
+	}
+	else if (letter === 'C') {
+		result = 'starts with C';
+	}
+	else if (letter === 'D') {
+		result = 'starts with D';
+	}
+	else if (letter === 'E') {
+		result = 'starts with E';
+	}
+	else {
+		result = 'starts with something else';
+	}
+	return result;
+}
+
+
+
+
+function nSum(input) {
+	var sum = 0;
+    for(var i=1; i<=input; i++) {	
+        sum += i;
+    }
+    return sum;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
